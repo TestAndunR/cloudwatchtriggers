a4f413d59d7445916d9743bfb47271a4f9a0f59f
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-07-10 14:32:48 +0530

    add a kinesis

--- cloudwatchtriggers/lambda.js
+++ cloudwatchtriggers/lambda.js
@@ -1,6 +1,22 @@
 let AWS = require('aws-sdk');
+const kinesis = new AWS.Kinesis();
+const ddb = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = function(event, context, callback) {
-    consoloe.log("Define authChallenge")
-    callback(null,'Successfully executed');
+exports.handler = function (event, context, callback) {
+    console.log("Define authChallenge")
+
+    kinesis.describeStream({
+        StreamName: 'testcafe'
+    }).promise()
+        .then(data => {
+            // your logic goes here
+            console.log(data)
+        })
+        .catch(err => {
+            // error handling goes here
+            console.log(err)
+        });
+
+
+    callback(null, 'Successfully executed');
 }
